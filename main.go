33e35567c11e0d4048ea6c69cfe398cc2a847794
Cassio Moreira <moreira@hp.com>
2021-08-19 17:15:28 +0000

    implement for - thank you Lucas

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 //assumes you have the following environment variables setup for AWS session creation
-// AWS_SDK_LOAD_CONFIG=1
-// AWS_ACCESS_KEY_ID=XXXXXXXXXX
-// AWS_SECRET_ACCESS_KEY=XXXXXXXX
-// AWS_REGION=us-west-2
+// export AWS_SDK_LOAD_CONFIG=1
+// export AWS_ACCESS_KEY_ID=XXXXXXXXXX
+// export  AWS_SECRET_ACCESS_KEY=XXXXXXXX
+// export  AWS_REGION=us-west-2
 
 package main
 
 import (
-"fmt"
-"github.com/aws/aws-sdk-go/aws"
-"github.com/aws/aws-sdk-go/aws/session"
-"github.com/aws/aws-sdk-go/service/sns"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
 )
 
 func main() {
@@ -24,19 +24,27 @@ func main() {
 	svc := sns.New(sess)
 	fmt.Println("service created")
 
-	params := &sns.PublishInput{
-		Message: aws.String(""),
-		PhoneNumber: aws.String(""),
-	}
-	resp, err := svc.Publish(params)
-
-	if err != nil {
-		// Print the error, cast err to awserr.Error to get the Code and
-		// Message from an error.
-		fmt.Println(err.Error())
-		return
+	message := aws.String("Casamento Renata e Cassio: não esqueca de fazer o seu teste de covid, agende seu exame na Farmacia Santa Branca por R$67,99. https://bit.ly/3z4v1Ii")
+	phoneList := [...]string{
+		// read from list.txt or put here the list of numbers
 	}
 
-	// Pretty-print the response data.
-	fmt.Println(resp)
+	for _, phone := range phoneList {
+
+		params := &sns.PublishInput{
+			Message: message,
+			PhoneNumber: aws.String(phone),
+		}
+		resp, err := svc.Publish(params)
+
+		if err != nil {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+			return
+		}
+
+		// Pretty-print the response data.
+		fmt.Println(resp)
+	}
 }
